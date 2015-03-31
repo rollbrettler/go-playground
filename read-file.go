4ec67b1849e0fd5bcd519ef5826f1899649a9f41
Tim Petter <tim@timpetter.de>
2015-03-31 16:44:42 +0200

    Add file reader

---
+++ read-file.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	_ "io/ioutil"
+	"os"
+)
+
+func main() {
+	//f, err := ioutil.ReadFile("README.md")
+	f, err := os.Open("README.md")
+if err != nil {
+		return
+	}
+	fmt.Println(f.Name())
+}
