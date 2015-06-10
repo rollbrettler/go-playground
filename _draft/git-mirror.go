3546ca06a8e2311bf5f0b017da79aabf04894901
Tim Petter <tim@timpetter.de>
2015-06-10 16:47:54 +0200

    Update git mirror draft to check if the repo already clone

--- _draft/git-mirror.go
+++ _draft/git-mirror.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	_ "fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/libgit2/git2go"
 )
 
+var clone string = "./test"
+var repo string = "https://github.com/rollbrettler/go-playground.git"
+
 func main() {
 
-  var cloneOptions git.CloneOptions
-  cloneOptions.Bare = true
+	var cloneOptions git.CloneOptions
+	cloneOptions.Bare = true
 
-  if _, err := git.Clone("https://github.com/rollbrettler/go-playground.git", "./test", &cloneOptions); err != nil {
+	_, err := ioutil.ReadDir(clone)
+	if err != nil {
 		log.Println(err)
+
+		_, err := git.Clone(repo, clone, &cloneOptions)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		return
 	}
+	log.Println("Folder already cloned")
 }
