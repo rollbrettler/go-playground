051ff9bc07b8f6b3a38e6a3a7b904c8dd9321c9d
Tim Petter <tim@timpetter.de>
2015-06-12 21:49:56 +0200

    Cleanup git mirror draft

--- _draft/git-mirror.go
+++ _draft/git-mirror.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/libgit2/git2go"
 )
@@ -13,67 +14,99 @@ var repo string = "https://gitlab.com/rollbrettler/go-playground.git"
 
 func main() {
 
-	cloneOptions := &git.CloneOptions{
-		Bare: true,
-	}
-
 	_, err := ioutil.ReadDir(clone)
 	if err != nil {
-		log.Println(err)
+		log.Println("Directory not pressent")
 
-		repository, err := git.Clone(repo, clone, cloneOptions)
+		err := cloneRepository()
 		if err != nil {
 			log.Println(err)
-			return
+			os.Exit(1)
 		}
-		config, err := repository.Config()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		err = changeToMirrorConfig(*config)
-		if (err != nil) {
-			log.Println(err)
-		}
-		// err = updateRepository(*repository)
-		// if (err != nil) {
-		// 	log.Println(err)
-		// }
-		fmt.Printf("%v\n", config)
-		return
 	}
+
 	log.Println("Folder already cloned")
 
 	repository, err := git.OpenRepository(clone)
 	if err != nil {
 		log.Println(err)
+		os.Exit(1)
+	}
+
+	err = updateRepository(*repository)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	log.Println("Sucessfully updated")
+	os.Exit(0)
+}
+
+func cloneRepository() (err error) {
+
+	cloneOptions := &git.CloneOptions{
+		Bare: true,
+	}
+
+	repository, err := git.Clone(repo, clone, cloneOptions)
+	if err != nil {
+		log.Println(err)
 		return
 	}
-	err = updateRepository(*repository)
-	if (err != nil) {
+
+	config, err := repository.Config()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = changeToMirrorConfig(*config)
+	if err != nil {
 		log.Println(err)
 	}
+
+	err = updateRepository(*repository)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Printf("%v\n", &config)
+
+	return nil
 }
 
 func changeToMirrorConfig(config git.Config) (err error) {
+
 	fetch, err := config.LookupString("remote.origin.fetch")
-	if (err != nil) {
-		log.Println(err)
+	if err != nil {
+		return err
 	}
-	mirror, err := config.LookupString("remote.origin.mirror")
-	if (err != nil) {
-		log.Println(err)
+
+	mirror, err := config.LookupBool("remote.origin.mirror")
+	if err != nil {
+		return err
 	}
-	fmt.Printf("%v %v\n", fetch, mirror)
+
+	log.Println(fmt.Sprintf("Refspec: %v\n", fetch))
 	config.SetString("remote.origin.fetch", "+refs/*:refs/*")
+
+	log.Println(fmt.Sprintf("Mirror: %v\n", mirror))
 	config.SetBool("remote.origin.mirror", true)
+
 	return nil
 }
 
 func updateRepository(repository git.Repository) (err error) {
+
 	remote, err := repository.LookupRemote("origin")
 	defer remote.Free()
+
 	refspecs := []string{"+refs/*:refs/*"}
-	remote.Fetch(refspecs ,nil, "")
+	err = remote.Fetch(refspecs, nil, "")
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
