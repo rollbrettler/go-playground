966b35e5088fa6e0bd3e4ada957c1f5412f56170
Tim Petter <tim@timpetter.de>
2015-09-08 16:26:03 +0200

    Format and update http example

--- _draft/simple-http.go
+++ _draft/simple-http.go
@@ -1,35 +1,33 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "net/http"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 type JsonData struct {
-  Name string `json:"name"`
-  Struct struct {
-    Integer int `json:"integer"`
-  } `json:"struce"`
+	Name   string `json:"name"`
+	Struct struct {
+		Integer int `json:"integer"`
+	} `json:"struce"`
 }
 
 func main() {
-  http.HandleFunc("/", hello)
-  http.ListenAndServe(":8001", nil)
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8001", nil)
 }
 
-func hello(writer http.ResponseWriter, reader *http.Request) {
-  file, err := ioutil.ReadFile("simple-http.json")
-  if err != nil {
-    fmt.Println(err)
-  }
+func hello(w http.ResponseWriter, r *http.Request) {
+	file, err := ioutil.ReadFile("simple-http.json")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-  var data JsonData
+	var data JsonData
 
-  decoder := json.NewDecoder(file)
+	json.Unmarshal(file, &data)
 
-  decoded := decoder.Decode(&data)
-
-  writer.Write(decoded)
+	w.Write(data)
 }
