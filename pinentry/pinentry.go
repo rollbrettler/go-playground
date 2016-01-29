323b61ba2ed3a8f4f2c91a9c015be8cdfdbcb5dd
Tim Petter <tim@timpetter.de>
2016-01-29 12:21:31 +0100

    Add web pinentry example

---
+++ pinentry/pinentry.go
@@ -0,0 +1,37 @@
+// go build pinetry.go
+// encfs --extpass=./pinentry $(pwd)/encfs-test/enc $(pwd)/encfs-test/dec
+// curl -X POST -d "{\"password\": \"Start\"}" http://localhost:8001
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type password struct {
+	Password string
+}
+
+func main() {
+	http.HandleFunc("/", pinentry)
+	http.ListenAndServe(":8001", nil)
+}
+
+func pinentry(w http.ResponseWriter, r *http.Request) {
+
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+	var t password
+	err := decoder.Decode(&t)
+	if err != nil {
+		w.Write([]byte("{'error':'cannot parse json'}"))
+	}
+
+	w.Write([]byte("{}"))
+	fmt.Println(t.Password)
+
+	os.Exit(0)
+}
