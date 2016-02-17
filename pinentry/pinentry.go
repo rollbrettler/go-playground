036716065d8c713101a8ff5d264677845406e375
Tim Petter <tim@timpetter.de>
2016-02-17 15:35:04 +0100

    Pinentry – Add html form to submit password via browser

--- pinentry/pinentry.go
+++ pinentry/pinentry.go
@@ -9,7 +9,24 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"text/template"
 )
+
+var htmlTemplate = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pinentry</title>
+  </head>
+  <body>
+    <form action="/" method="post">
+			<input name="password" />
+			<button>Submit pin</button>
+		</form>
+  </body>
+</html>
+`
 
 type password struct {
 	Password string
@@ -24,21 +41,33 @@ func pinentry(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "GET":
+		t, _ := template.New("html").Parse(htmlTemplate)
+		t.Execute(w, nil)
 	case "POST":
 		handlePost(w, r)
 	}
 }
 
 func handlePost(w http.ResponseWriter, r *http.Request) {
+
+	// If the form was send via http post use this
+	if r.PostFormValue("password") != "" {
+		fmt.Println(r.PostFormValue("password"))
+		w.Write([]byte("password was submitted"))
+		os.Exit(0)
+	}
+
+	// Try to fetch the password paramerter from the json body
 	decoder := json.NewDecoder(r.Body)
 	defer r.Body.Close()
 	var t password
 	err := decoder.Decode(&t)
 	if err != nil {
 		w.Write([]byte("{'error':'cannot parse json'}"))
+		return
 	}
 
-	w.Write([]byte("{}"))
+	w.Write([]byte("{'message':'password was submitted'}"))
 	fmt.Println(t.Password)
 
 	os.Exit(0)
